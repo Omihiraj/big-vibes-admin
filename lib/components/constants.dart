48357afa71c5ffdf108b9d0cf491493e178a2610
omihiraj <mihirajwijesooriya@gmail.com>
2022-08-09 20:51:17 +0530

    Color Change

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF63A0A3);
-const secondaryColor = Color(0xFF06283B);
+const primaryColor = Color(0xFF00DDF7);
+const secondaryColor = Color(0xFFFEF473);
 const Color bgColor = Color.fromRGBO(243, 243, 243, 1);
 const double headlineText = 32;
